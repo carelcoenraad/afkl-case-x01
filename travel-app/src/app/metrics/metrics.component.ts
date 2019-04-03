68a4da6b50a6b7d70d66e42885b1680780d7e505
Carel Coenraad <carel.coenraad@capgemini.com>
2019-04-03 15:54:58 +0200

    feat: add simple dashboard for metrics

--- travel-app/src/app/metrics/metrics.component.ts
+++ travel-app/src/app/metrics/metrics.component.ts
@@ -7,11 +7,29 @@ import { MetricsService } from '../metrics.service';
   styleUrls: ['./metrics.component.sass'],
 })
 export class MetricsComponent implements OnInit {
-  trace: any;
+  apiTraces = [];
+  clientErrors = [];
+  okResponses = [];
+  responseTimeAvg: number;
+  responseTimeMax: number;
+  responseTimeMin: number;
+  serverErrors = [];
 
   constructor(private metricsService: MetricsService) {}
 
   ngOnInit() {
-    this.metricsService.getHttpTrace().subscribe(trace => (this.trace = trace));
+    this.metricsService.getHttpTrace().subscribe(response => this.filterMetrics(response.traces));
   }
+
+  filterMetrics = traces => {
+    this.apiTraces = traces.filter(trace => trace.request.uri.includes('/api'));
+    const statuses = this.apiTraces.map(trace => trace.response.status);
+    const responseTimes = this.apiTraces.map(trace => trace.timeTaken);
+    this.okResponses = statuses.filter(status => status === 200);
+    this.clientErrors = statuses.filter(status => status >= 400 && status <= 499);
+    this.serverErrors = statuses.filter(status => status >= 500 && status <= 500);
+    this.responseTimeAvg = Math.floor(responseTimes.reduce((acc, cur) => acc + cur, 0) / responseTimes.length);
+    this.responseTimeMax = Math.max(...responseTimes);
+    this.responseTimeMin = Math.min(...responseTimes);
+  };
 }
